c739e5c52bf2fb579177f81e9c18d0d6d451fa69
jo <joseph.touzet@ens-paris-saclay.fr>
2024-07-10 12:24:57 +0200

    update

--- cmds/desktop/rename-all.sh
+++ cmds/desktop/rename-all.sh
@@ -31,7 +31,7 @@ Usage: \"rename-all -b/e -r/f [patern to replace] [patern to replace with]\"
 
 	-r recursive (not yet supported)
 	-b add patern to begin of file name
-	-e add patern to end of file name
+	-e add patern to end of file name (before extension)
 	-R replace following patern with the given patern
 	-f patern to find to add the patern, without replacing it
 	-E replace with empty patern
@@ -44,18 +44,18 @@ to_replace=
 to_find=
 empty=false
 
-while getopts 'hbeR:f:E' flag; do
+while getopts 'hrbeR:f:E' flag; do
 	case "${flag}" in
 		h) print_usage;
 			exit 1;;
     	r) recursive=true;;
-		b) if [ "${place}" = "end" ] || [ ! -z "${to_find}" ] || [ ! -z "${to_replace}" ]; then
-			>&2 echo "Can't add to end (-e), replace (-R), or find (-f) AND begin (-b) !"
+		b) if [ "${place}" = "end" ] || [ ! -z "${to_replace}" ]; then
+			>&2 echo "Can't add to end (-e) or replace (-R) AND begin (-b) !"
 			exit 1
 		fi
 		place=begin;;
-		e) if [ "${place}" = "begin" ] || [ ! -z "${to_find}" ] || [ ! -z "${to_replace}" ]; then
-			>&2 echo "Can't add to begin (-b), replace (-R), or find (-f) AND end (-e) !"
+		e) if [ "${place}" = "begin" ] || [ ! -z "${to_replace}" ]; then
+			>&2 echo "Can't add to begin (-b) or replace (-R) AND end (-e) !"
 			exit 1
 		fi
 		place=end;;
@@ -64,8 +64,8 @@ while getopts 'hbeR:f:E' flag; do
 			exit 1
 		fi
 		to_replace="${OPTARG}";;
-		f) if [ "${place}" = "end" ] || [ "${place}" = "begin" ] || [ ! -z "${to_replace}" ]; then
-			>&2 echo "Can't add to begin (-b), end (-e), or replace (-R) AND find (-f) !!"
+		f) if [ ! -z "${to_replace}" ]; then
+			>&2 echo "Can't replace (-R) AND find (-f) !!"
 			exit 1
 		fi
 		to_find="${OPTARG}";;
@@ -78,11 +78,65 @@ done
 to_add=
 if [ "${empty}" = false ]; then
 	to_add="${@: -1}"
+	if [ -z "${to_add}" ]; then
+		>&2 echo "No patern to replace with/add given !"
+		exit 1
+	fi
 fi
 
-if [ -z "${to_add}" ] && [ "${empty}" = false ]; then
-	>&2 echo "No patern to replace with/add given !"
-	exit 1
-fi
+read -p "Are you sure you want to continue? [Y|n] " prompt
 
-echo "${place} ${to_replace}/${to_find} ${to_add}"
+recursion()
+{
+    for file in `find "." -maxdepth 1`; do
+    	if [ "$file" == "." ]; then
+    		continue
+    	fi
+
+    	if [ ! -z "${to_find}" ]; then
+    		if [ -z "$(echo "${file}" | grep ${to_find} )" ]; then
+    			if [[ -d ${file} ]] && [ ${recursive} = true ]; then
+					cd ${file}
+					recursion
+					cd ../
+				fi
+    			continue
+    		fi
+		fi
+
+		outfile="${file}"
+		if [ "${place}" = "begin" ]; then
+			outfile="./${to_add}${file:2}"
+		elif [ "${place}" = "end" ]; then
+			filename="${file:2}"
+			extension="${filename##*.}"
+			filename="${filename%.*}"
+
+			outfile="./${filename}${to_add}.${extension}"
+		else
+			if [ ! -z "${to_replace}" ]; then
+				outfile="${outfile/"${to_replace}"/"${to_add}"}"
+			fi
+		fi
+
+		if [ "${file}" != "${outfile}" ]; then
+			mv "$file" "$outfile"
+			echo "mv $file $outfile"
+		fi
+
+		if [[ -d ${outfile} ]] && [ ${recursive} = true ]; then
+			cd $outfile
+			recursion
+			cd ../
+		fi
+	done
+}
+
+if [[ $prompt == "Y" ]]; then
+	OIFS="$IFS"
+	IFS=$'\n'
+	recursion
+	IFS=$OIFS
+else
+	exit 0
+fi
